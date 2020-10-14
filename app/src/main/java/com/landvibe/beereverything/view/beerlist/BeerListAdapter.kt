da400c8087ab139c96a51fa4ec9898dd3e58bf69
kyujeong <heo0414@naver.com>
2020-10-14 22:18:37 +0900

    Add beer image and category in beer list item

--- app/src/main/java/com/landvibe/beereverything/view/beerlist/BeerListAdapter.kt
+++ app/src/main/java/com/landvibe/beereverything/view/beerlist/BeerListAdapter.kt
@@ -2,8 +2,12 @@ package com.landvibe.beereverything.view.beerlist
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
 import androidx.paging.PagedListAdapter
 import androidx.recyclerview.widget.DiffUtil
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.landvibe.beereverything.data.Beer
 import com.landvibe.beereverything.databinding.ItemBeerlistBinding
 
@@ -31,6 +35,17 @@ class BeerListAdapter(private val itemClickListener: OnItemClickListener) :
 
             override fun areContentsTheSame(oldItem: Beer, newItem: Beer): Boolean =
                 oldItem.name == newItem.name && newItem.image_url == oldItem.image_url
+        }
+
+        @BindingAdapter("imageFromUrl")
+        @JvmStatic
+        fun bindImageFromUrl(view: ImageView, imageUrl: String?) {
+            if (!imageUrl.isNullOrEmpty()) {
+                Glide.with(view.context)
+                    .load(imageUrl)
+                    .transition(DrawableTransitionOptions.withCrossFade())
+                    .into(view)
+            }
         }
     }
 
