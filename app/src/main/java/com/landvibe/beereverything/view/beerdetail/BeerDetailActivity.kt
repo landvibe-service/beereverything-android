c97ade8dd4f67cdebbf849191b5de576deb8ca82
kyujeong <heo0414@naver.com>
2020-10-14 23:47:49 +0900

    Add beer data in detail activity

--- app/src/main/java/com/landvibe/beereverything/view/beerdetail/BeerDetailActivity.kt
+++ app/src/main/java/com/landvibe/beereverything/view/beerdetail/BeerDetailActivity.kt
@@ -1,38 +1,26 @@
 package com.landvibe.beereverything.view.beerdetail
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.landvibe.beereverything.R
+import com.landvibe.beereverything.data.Beer
 import com.landvibe.beereverything.databinding.ActivityBeerdetailBinding
 
 class BeerDetailActivity : AppCompatActivity() {
     private lateinit var beerViewModel: BeerDetailViewModel
-    private var beerId = BEER_DEFAULT_ID
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding: ActivityBeerdetailBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_beerdetail)
         beerViewModel = ViewModelProvider(this).get(BeerDetailViewModel::class.java)
+        binding.beerItem = intent.getSerializableExtra(EXTRA_BEER_DATA) as Beer
         binding.viewmodel = beerViewModel
 
         observeLiveData()
-        loadBeer()
-    }
-
-
-    private fun loadBeer() {
-        beerId = intent.getIntExtra(BEER_ID_KEY, BEER_DEFAULT_ID)
-        if (beerId == BEER_DEFAULT_ID) {
-            Toast.makeText(this, "not found", Toast.LENGTH_LONG).show()
-            finish()
-        }
-
-        beerViewModel.loadBeer(beerId)
     }
 
     private fun observeLiveData() {
@@ -44,7 +32,6 @@ class BeerDetailActivity : AppCompatActivity() {
     }
 
     companion object {
-        const val BEER_ID_KEY = "beer_id"
-        private const val BEER_DEFAULT_ID = 0
+        const val EXTRA_BEER_DATA = "beer_data"
     }
 }
